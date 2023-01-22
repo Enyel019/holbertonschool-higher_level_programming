fb187db6148f72f87f3283f1c5e81a522f09eb64
Enyel019 <[el_dominic01@hotmail.com]>
2023-01-22 17:21:48 -0400

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 90):
-    if i == 89:
-        print('{:02d}'.format(i))
-    else:
-        print('{:02d}, ' .format(i), end=" ")
+for i in range(0, 100):
+    if i % 10 > i / 10:
+        if i == 89:
+            print('{:02d}'.format(i))
+        else:
+            print('{:02d}, ' .format(i), end="")

5fba21386cdb357e266a5ea26ee3a5afdebf55b6
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-02 19:32:43 -0400

    4. Replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    mylist = list.copy(my_list)
+    if (idx < 0 or idx > len(mylist)):
+        return mylist
+    for count in range(len(mylist)):
+        if (count == idx):
+            mylist[idx] = element
+            return mylist

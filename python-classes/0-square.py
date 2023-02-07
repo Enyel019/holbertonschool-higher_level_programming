67a4034a5f21c2a849a35cfc7e8b87cd62f4c2ce
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-06 21:06:51 -0400

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Write an empty class Square"""
 
-
-""" Square is a class that has no methods or attributes"""
-
-
 class Square:
+# It creates a class called Square.
+
     pass

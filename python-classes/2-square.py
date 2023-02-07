b0be220da6395e3e8af26b7e4fef42846da1397f
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-06 22:45:00 -0400

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+    """Write a class Square that defines a square by.
 
+
+    Raises:
+        TypeError: ("size must be an integer")
+        ValueError: ("size must be >= 0")
+    """
 
 class Square:
 

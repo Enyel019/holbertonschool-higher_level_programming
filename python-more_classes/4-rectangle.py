610d8b7f75dde31ab01395a20c2ebc5f13b00d35
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-13 23:33:39 -0400

    4. Eval is magic

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by
+"""
+
+
+class Rectangle:
+    """
+    A rectangle has a width, a height, an area, and a perimeter.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        s = ''
+        if self.__width == 0 or self.__height == 0:
+            return s
+        for c in range(self.__height):
+            for f in range(self.__width):
+                if f == self.__width - 1 and c != self.__height - 1:
+                    s += "#\n"
+                else:
+                    s += "#"
+        return s
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)

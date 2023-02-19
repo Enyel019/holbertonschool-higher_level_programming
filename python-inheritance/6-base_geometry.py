372d0e4ad733b68a5f93f1c30914ae87b2b22966
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-19 16:41:36 -0400

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry():
+    """
+    This class is a base class for all geometries
+
+    """
+    def area(self):
+        """
+        the raise an error
+        """
+        raise Exception("area() is not implemented")

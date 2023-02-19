679162ff5e56f971be56ad96f87050e1dbdeaf50
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-19 14:11:39 -0400

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Write a class MyList that inherits from list.
+"""
+
+
 class MyList(list):
+
     """It's a list that can be sorted by the second element of
     each tuple in the list
     """
+
     def print_sorted(self):
         """It prints the elements of the list in sorted order.
         """

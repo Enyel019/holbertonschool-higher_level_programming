78bd58235f66b56bab9bc43c6e1f514f87bceefb
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-20 20:48:34 -0400

    new

---
+++ python-inheritance/tests/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry():
+    """
+    This class is a base class for all geometries
+
+    """
+    def area(self):
+        raise Exception("area() is not implemen")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{:s} must be greater than 0".format(name))

0c91d294897d54e510f862a58485abf1388f3f45
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-20 23:52:56 -0400

    11. Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Importing the class Rectangle from the file 9-rectangle.py"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    rectangle class
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ('[Square] {:d}/{:d}'.format(self.__size, self.__size))

8ab4a3c51428b0b1f9c8ec7352ea19bc7098d584
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-22 21:14:48 -0400

    3. To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a function that returns the JSON representation of an object (string):
+"""
+
+
+def to_json_string(my_obj):
+    """
+    It converts a python object to a json string.
+
+    :param my_obj: the object to be serialized
+    """
+
+    return (my_obj)

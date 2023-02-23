4c12847f67cdb73753ce3e345738b56986b8a088
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-22 20:10:37 -0400

    0. Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin//python3
+#!/usr/bin/python3
 """Write a function that reads a text file (UTF8) and prints it to stdout:
 """
 
---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a function that writes a string to a text file (UTF8)
+   and returns the number of characters written:
+"""
+
+
+def write_file(filename="", text=""):
+    

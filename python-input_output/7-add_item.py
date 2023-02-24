de5cec0d804d2a832efbbf7981bc32e107d93fe2
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-23 20:54:18 -0400

    new

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,14 +8,16 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# Cargar la lista existente o crear una nueva si no existe el archivo
+# This is a try/except block. If the file add_item.json exists,
+#         it will load the list from the file. If the file does not exist,
+#         it will create an empty list.
 try:
     list = load_from_json_file('add_item.json')
 except FileNotFoundError:
     list = []
 
-# Agregar los argumentos a la lista
+# Adding the arguments to the list.
 list.extend(sys.argv[1:])
 
-# Guardar la lista en un archivo JSON
+# Saving the list to a file named add_item.json.
 save_to_json_file(list, 'add_item.json')

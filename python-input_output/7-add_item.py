79bf53c9d48878935240f85fae992b3e878ac4dc
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-23 20:48:14 -0400

    new

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,23 +5,17 @@
 
 
 import sys
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-"""
- Importing the functions load_from_json_file and save_to_json_file from the files
- 8-load_from_json_file.py and 7-save_to_json_file.py.
-"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# Trying to load the file add_item.json and if it does not exist,
-# it will create it.
+# Cargar la lista existente o crear una nueva si no existe el archivo
 try:
     list = load_from_json_file('add_item.json')
-# If the file does not exist, it will create it.
-except:
+except FileNotFoundError:
     list = []
 
-# Adding the arguments to the list.
-list.append(sys.argv[1:])
+# Agregar los argumentos a la lista
+list.extend(sys.argv[1:])
 
-# Saving the list into the file add_item.json.
+# Guardar la lista en un archivo JSON
 save_to_json_file(list, 'add_item.json')

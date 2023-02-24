8206681411016f1c0f1594db27b32d4252a68f47
Enyel019 <[el_dominic01@hotmail.com]>
2023-02-23 22:47:50 -0400

    9. Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by:
+"""
+
+
+class Student():
+
+    def __init__(self, first_name, last_name, age):
+        """
+        This function takes in three parameters (first_name, last_name, age)
+         and assigns them to the
+        instance variables first_name, last_name, and age.
+
+        :param first_name: The first name of the person
+        :param last_name: The last name of the person
+         :param age: The age of the person
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__

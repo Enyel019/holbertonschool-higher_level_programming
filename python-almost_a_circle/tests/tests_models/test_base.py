72cf4ef9ef543180879fd71227ef8c830de960cf
Enyel019 <[el_dominic01@hotmail.com]>
2023-03-01 23:58:42 -0400

    prueba

---
+++ python-almost_a_circle/tests/tests_models/test_base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Base tests
+"""
+from models.base import Base
+import unittest
+import os
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        """setting up for each test """
+        Base._Base__nb_objects = 0
+
+    def test_id(self):
+        """ checks if correct id was generated"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 4)

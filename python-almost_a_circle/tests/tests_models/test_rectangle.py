3f1ab9b896aff0037a3214fb88c07983202c460c
Enyel019 <[el_dominic01@hotmail.com]>
2023-03-02 21:26:51 -0400

    fix

---
+++ python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""set of tests rectangle class"""
+
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+import os
+
+
+class RectangleTest(unittest.TestCase):
+    """Tests rectangle class"""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_docstring(self):
+        """checks for any docstring"""
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
+
+    def tearDown(self):
+        if os.path.exists("Base.json"):
+            os.remove("Base.json")
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")

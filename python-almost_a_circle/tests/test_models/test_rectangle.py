6bf732889e92e99a61ec9db53133c40ca832bc8a
Enyel019 <[el_dominic01@hotmail.com]>
2023-03-04 16:21:31 -0400

    update

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Base tests
+"""
+from models.rectangle import Rectangle
+from models.base import Base
+import unittest
+import os
+
+
+class RectangleTest(unittest.TestCase):
+    """The `BaseTest` class is a subclass of the `unittest.TestCase` class
+    """
+
+
+def setUp(self):
+    """setting up for each test """
+    Base._Base__nb_objects = 0
+
+
+def test_any_docstring(self):
+    """Checks for any docstring"""
+    self.assertTrue(len(Base.__doc__) >= 1)

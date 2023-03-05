95844128090cb15753bfbf6ed935149c6f05c59d
Enyel019 <[el_dominic01@hotmail.com]>
2023-03-04 21:28:37 -0400

    new test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-"""Base tests
-"""
+
+"""set of tests rectangle class"""
+
+import unittest
 from models.rectangle import Rectangle
 from models.base import Base
-import unittest
 import os
 
 
 class RectangleTest(unittest.TestCase):
-    """The `BaseTest` class is a subclass of the `unittest.TestCase` class
-    """
+    """Tests rectangle class"""
 
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
-def setUp(self):
-    """setting up for each test """
-    Base._Base__nb_objects = 0
+    def test_docstring(self):
+        """checks for any docstring"""
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
 
+    def tearDown(self):
+        if os.path.exists("Base.json"):
+            os.remove("Base.json")
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")
 
-def test_docstring(self):
-    """checks for any docstring"""
-    self.assertTrue(len(Rectangle.__doc__) >= 1)
-
-
-def tearDown(self):
-    if os.path.exists("Base.json"):
-        os.remove("Base.json")
-    if os.path.exists("Rectangle.json"):
-        os.remove("Rectangle.json")
-    if os.path.exists("Square.json"):
-        os.remove("Square.json")
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_class(self):
+        '''Testsclass'''
+        self.assertEqual(str(Rectangle),
+                         "<class 'models.rectangle.Rectangle'>")

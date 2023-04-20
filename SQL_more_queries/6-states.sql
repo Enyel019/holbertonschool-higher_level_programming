d7fb9d4022c6b08d29620bc0f50905e9316f3e89
Enyel019 <[el_dominic01@hotmail.com]>
2023-04-20 19:36:18 -0400

    6. States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+       (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY  UNIQUE ,
+       	name varchar(256) NOT NULL);

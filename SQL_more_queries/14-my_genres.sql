190c43a6084b32abd1f36f32ad245490718f0bae
Enyel019 <[el_dominic01@hotmail.com]>
2023-04-24 20:17:41 -0400

    update

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,4 @@ ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;
+ORDER BY tv_genres.name ASC;

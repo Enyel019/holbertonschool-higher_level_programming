3f17286f558d5676af70805846f9b8de4dbea89f
Enyel019 <[el_dominic01@hotmail.com]>
2023-05-01 19:38:45 -0400

    7. All states via SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""connect database."""
+import MySQLdb
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+"""
+create
+class to relate with
+database
+"""
+if __name__ == "__main__":
+    try:
+        if sys.argv[3]:
+            engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+            Base.metadata.create_all(engine)
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            for states in session.query(State).order_by(State.id).all():
+                print("{}: {}".format(states.id, states.name))
+            session.close()
+    except MySQLdb.Error:
+        pass

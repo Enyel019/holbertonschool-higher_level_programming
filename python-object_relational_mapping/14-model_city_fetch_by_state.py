bd10235b02f3046ac302ffa535c6d00ad810f720
Enyel019 <[el_dominic01@hotmail.com]>
2023-05-03 19:35:50 -0400

    update

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
             session = Session()
 
             for state, city in session.query(State, City).filter(
-                State.id == City.state_id)\
-                    .order_by(City.id).all():
+                    State.id == City.state_id).\
+                    order_by(City.id).all():
                 print("{}: ({}) {}".format(state.name, city.id, city.name))
             session.close()
     except MySQLdb.Error:

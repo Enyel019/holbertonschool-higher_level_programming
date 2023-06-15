376e3fdebf6300e4bc460ac0d1a83a3e2de3e0b5
Enyel019 <el_dominic01@hotmail.com>
2023-06-14 21:18:30 -0400

    new update

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
 """connect database."""
-import MySQLdb
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-"""
-create
-class to relate with
-database
-"""
-if __name__ == "__main__":
-    try:
-        if sys.argv[3]:
-            engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+import sys 
 
-            Base.metadata.create_all(engine)
-            Session = sessionmaker(bind=engine)
-            session = Session()
+if __name__ == '__main__':
+    # Crear el motor de base de datos y la sesión
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-            up_state = session.query(State).get(2)
-            up_state.name = 'New Mexico'
-            session.commit()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-            session.close()
-    except MySQLdb.Error:
-        pass
+    # Obtener el objeto State con id igual a 2 utilizando Session.get()
+    up_state = session.get(State, 2)
+
+    if up_state:
+        up_state.name = 'New Mexico'
+        session.commit()
+
+    session.close()
+ 

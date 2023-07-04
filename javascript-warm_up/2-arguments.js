b823a69bc6ec4a59b01a5782ff8781a8cabca7f9
Enyel019 <el_dominic01@hotmail.com>
2023-07-04 18:38:34 -0400

    2. Arguments update

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+// Get the number of arguments passed to the script
 const argsCount = process.argv.length - 2;
+// Check the number of arguments and display a corresponding message
 if (argsCount === 0) {
     console.log('No argument');
 } else if (argsCount === 1) {

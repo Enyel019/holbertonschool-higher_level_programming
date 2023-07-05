fa88d4fc11cb96eb1353a15422d7532f4dc9e483
Enyel019 <el_dominic01@hotmail.com>
2023-07-05 19:23:57 -0400

    10. Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const factorial = (n) => {
   if (isNaN(n)) {
-    return 1
+    return 1;
   }
   if (n === 0) {
-    return 1
+    return 1;
   }
   return n * factorial(n - 1);
-}
-const number = parseInt(process.argv[2]);
+};
+ const number = parseInt(process.argv[2]);
 console.log(`${factorial(number)}`);

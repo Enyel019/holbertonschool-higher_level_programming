0cebf6ca064f4143390378ee10628e104b735074
Enyel019 <el_dominic01@hotmail.com>
2023-07-04 21:27:43 -0400

    6. Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-lines.forEach((line) => {
-console.log(line)
-})
+#!/usr/bin/env node
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) {
+  console.log(line);
+}

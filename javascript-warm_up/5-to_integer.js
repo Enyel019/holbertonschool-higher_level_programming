0713e2f568dee4dc5ae8d3675d7676d2f8426468
Enyel019 <el_dominic01@hotmail.com>
2023-07-04 21:15:40 -0400

    new update

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const inputNumber = parseInt(process.argv[2], 10)
+const inputNumber = parseInt(process.argv[2], 10);
 if (isNaN(inputNumber)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
-  console.log(`My number: ${inputNumber}`)
+  console.log(`My number: ${inputNumber}`);
 }
--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+#!/usr/bin/env node
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 lines.forEach(line => {
-  console.log(line)
-})
+  console.log(line);
+});

31f1b8102311a24388d7d0a80dad6a0384edf754
Enyel019 <el_dominic01@hotmail.com>
2023-07-04 22:35:21 -0400

    9. Add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const add = (a, b) => parseInt(a) + parseInt(b)
+const add = (a, b) => parseInt(a) + parseInt(b);
 
-const firstNumber = process.argv[2]
-const secondNumber = process.argv[3]
+const firstNumber = process.argv[2];
+const secondNumber = process.argv[3];
 
-console.log(add(firstNumber, secondNumber))
+console.log(add(firstNumber, secondNumber));

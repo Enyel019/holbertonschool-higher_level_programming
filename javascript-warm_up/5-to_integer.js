5be685bb5945bfe49f25c338f11ec8db0c5db580
Enyel019 <el_dominic01@hotmail.com>
2023-07-04 20:34:41 -0400

    5. An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
 if (parseInt(process.argv.slice(2)[0])) {
-  console.log('My number: %i', parseInt(process.argv.slice(2)[0]))
+    console.log('My number: %i', parseInt(process.argv.slice(2)[0]));
 } else {
-  console.log('Not a number')
+    console.log('Not a number');
 }

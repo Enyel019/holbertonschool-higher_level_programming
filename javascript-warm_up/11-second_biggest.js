de8470b4ee35ad8d9ed2187ad10732c44223f984
Enyel019 <el_dominic01@hotmail.com>
2023-07-05 22:37:36 -0400

    new update

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log('0');
-} else if (args.length === 1) {
-  console.log('0');
-}
-if (args.length > 1) {
-  const sortedArgs = args.sort();
-  const secondLastArg = sortedArgs[sortedArgs.length - 2];
-  console.log(secondLastArg);
-}
+const args = process.argv.slice(2).map(Number);
+const secondLargest = args.sort((a, b) => b - a)[1] || 0;
+console.log(secondLargest);

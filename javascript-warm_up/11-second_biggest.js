e952539faca995a48e02bb834b54f606d82f72b1
Enyel019 <el_dominic01@hotmail.com>
2023-07-05 21:21:04 -0400

    11. Second biggest!

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const i = process.argv.slice(2);
+if (process.argv.slice(2).length === 0) {
+  console.log('0');
+} else if (process.argv.slice(2).length === 1) {
+  console.log('1');
+}
+if (process.argv.slice(2).length > 1) {
+  const s = i.sort().length;
+  console.log(i[s - 2]);
+}

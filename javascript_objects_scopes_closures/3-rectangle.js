54eded6d4d0d2a149d06f8e6d02d9e90d15c5ec8
Enyel019 <el_dominic01@hotmail.com>
2023-07-06 21:45:16 -0400

    3. Rectangle #3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    const row = "X".repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;

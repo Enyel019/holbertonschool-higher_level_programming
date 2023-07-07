8f98b3bdc2761c894dcce301edefe0fc0bab069d
Enyel019 <el_dominic01@hotmail.com>
2023-07-07 19:41:09 -0400

    4. Rectangle #4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -13,6 +13,17 @@ class Rectangle {
       console.log("X".repeat(this.width));
     }
   }
+
+  rotate() {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double() {
+    this.height *= 2;
+    this.width *= 2;
+  }
 }
 
 module.exports = Rectangle;

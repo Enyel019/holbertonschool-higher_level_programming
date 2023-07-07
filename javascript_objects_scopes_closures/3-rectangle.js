69ca2fb110ce874961859580080e5c4d646a9b77
Enyel019 <el_dominic01@hotmail.com>
2023-07-06 22:00:24 -0400

    new update

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-   print() {
+  print() {
     const row = "X".repeat(this.width);
     for (let i = 0; i < this.height; i++) {
       console.log(row);

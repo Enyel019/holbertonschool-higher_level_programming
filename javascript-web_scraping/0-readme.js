c3233e068357bfdb93bc5f770490a4d9b8246873
Enyel019 <el_dominic01@hotmail.com>
2023-07-09 14:08:28 -0400

    0. Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf-8", (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     throw err;
   }

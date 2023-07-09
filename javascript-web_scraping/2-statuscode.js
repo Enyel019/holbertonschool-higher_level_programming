49b959f3a32edf4d8099db609fb46a9dbf9ccbf4
Enyel019 <el_dominic01@hotmail.com>
2023-07-09 15:06:31 -0400

    2. Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const request = require('request');
-
-request(url, function (err, res, body) {
-  if (err) {
-    throw (err);
-  }
-  console.log('code: ${res.statusCode}');
-});
+request(url)
+  .on('response', function (response) {
+    console.log('code: %s', response.statusCode.toString());
+  });

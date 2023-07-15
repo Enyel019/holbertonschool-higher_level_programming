b99ef7ff158e980d03336877af11af9dbc70e4cf
Enyel019 <el_dominic01@hotmail.com>
2023-07-15 11:09:39 -0400

    new update

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 const doc = window.$;
-doc.get('https://swapi.co/api/films/?format=json', function (data) {
-  for (const peli of data.results) {
-    $('#list_movies').append(`<li>${peli.title}</li>`);
-  }
-});
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
+    $.each(data.results, function(index, movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
